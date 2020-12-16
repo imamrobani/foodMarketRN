ad50b3910b3469f206d2de8966e6b3ad334a5408
Imam Robani <imamrob@gmail.com>
2020-12-16 22:17:22 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  Text
+} from 'react-native';
+import { SplashScreen } from './pages';
+
+
+const App = () => {
+  return (
+    <SplashScreen />
+  );
+};
+
+
+export default App;
