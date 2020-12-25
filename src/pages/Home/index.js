ac45ab6686f72e784edf4d3c4fea3e66e10d3e18
Imam Robani <imamrob@gmail.com>
2020-12-25 19:09:36 +0700

    add welcoming user

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,45 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import { ProfileDummy } from '../../assets'
+import Fonts from '../../const/Fonts'
 
 const Home = () => {
   return (
     <View>
-      <Text>Home Page</Text>
+      <View style={styles.profileContainer}>
+        <View>
+          <Text style={styles.appName}>FoodMarket</Text>
+          <Text style={styles.desc}>Let's get some foods</Text>
+        </View>
+        <Image source={ProfileDummy} style={styles.profile} />
+      </View>
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  profileContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 24,
+    paddingTop: 32,
+    paddingBottom: 24,
+    backgroundColor: 'white'
+  },
+  appName: {
+    fontSize: 22,
+    fontFamily: Fonts.POPPINS_MEDIUM,
+    color: '#020202'
+  },
+  desc: {
+    fontFamily: Fonts.POPPINS_LIGHT,
+    color: '#8D92A3'
+  },
+  profile: {
+    width: 50,
+    height: 50,
+    borderRadius: 8
+  }
+})
