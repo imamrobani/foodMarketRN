27ed28d7015e1aad2ad1db4b4733b4a2a5bf5e31
Imam Robani <imamrob@gmail.com>
2020-12-27 10:56:23 +0700

    make component Item List Food

---
+++ src/components/molecules/ItemListFood/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import { Rating } from '..'
+import Fonts from '../../../const/Fonts'
+
+const ItemListFood = ({ image }) => {
+  return (
+    <View style={{
+      flexDirection: 'row',
+      backgroundColor: 'white',
+      paddingHorizontal: 24,
+      paddingVertical: 8,
+      alignItems: 'center'
+    }}>
+      <Image
+        source={image}
+        style={{
+          width: 60,
+          height: 60,
+          borderRadius: 8,
+          overflow: 'hidden',
+          marginRight: 12
+        }} />
+      <View style={{ flex: 1 }}>
+        <Text style={{
+          fontFamily: Fonts.POPPINS_REGULAR,
+          fontSize: 16,
+          color: '#020202'
+        }}>Soup Bumil</Text>
+        <Text style={{
+          fontFamily: Fonts.POPPINS_REGULAR,
+          fontSize: 13,
+          color: '#8D92A3'
+        }}>IDR 289.000</Text>
+      </View>
+      <Rating />
+    </View>
+  )
+}
+
+export default ItemListFood
+
+const styles = StyleSheet.create({})
