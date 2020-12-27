0b5377938584f8728516accae8c953329b1baeb7
Imam Robani <imamrob@gmail.com>
2020-12-27 10:07:47 +0700

    customize swipeable tab view

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,79 @@
 import React from 'react'
-import { StyleSheet, Text, View, Image } from 'react-native'
-import { ProfileDummy } from '../../assets'
+import { StyleSheet, Text, View, Image, ScrollView, Dimensions } from 'react-native'
+import { FoodDummy1, ProfileDummy, FoodDummy2, FoodDummy3, FoodDummy4 } from '../../assets'
+import { FoodCard, Gap } from '../../components'
 import Fonts from '../../const/Fonts'
+import { TabView, SceneMap, TabBar } from 'react-native-tab-view'
+
+const renderTabBar = props => (
+  <TabBar
+    {...props}
+    indicatorStyle={{ backgroundColor: '#020202', height: 3, width: '15%', marginLeft: '3%' }}
+    style={{ backgroundColor: 'white' }}
+    tabStyle={{ width: 'auto' }}
+    renderLabel={({ route, focused, color }) => (
+      <Text style={{
+        fontFamily: Fonts.POPPINS_REGULAR,
+        color: focused ? '#020202' : '#8D92A3'
+      }}>
+        {route.title}
+      </Text>
+    )}
+  />
+);
+
+const FirstRoute = () => (
+  <View style={{ backgroundColor: '#ff4081', flex: 1 }} />
+);
+
+const SecondRoute = () => (
+  <View style={{ backgroundColor: '#673ab7', flex: 1 }} />
+);
+
+const initialLayout = { width: Dimensions.get('window').width }
 
 const Home = () => {
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: '1', title: 'New Taste' },
+    { key: '2', title: 'Popular' },
+    { key: '3', title: 'Recommended' },
+  ]);
+
+  const renderScene = SceneMap({
+    1: FirstRoute,
+    2: SecondRoute,
+    3: FirstRoute,
+  });
+
   return (
-    <View>
+    <View style={styles.page}>
       <View style={styles.profileContainer}>
         <View>
           <Text style={styles.appName}>FoodMarket</Text>
           <Text style={styles.desc}>Let's get some foods</Text>
         </View>
         <Image source={ProfileDummy} style={styles.profile} />
+      </View>
+      <View>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          <View style={styles.foodContainer}>
+            {/* <Gap widht={24} /> */}
+            <FoodCard image={FoodDummy1} />
+            <FoodCard image={FoodDummy3} />
+            <FoodCard image={FoodDummy2} />
+            <FoodCard image={FoodDummy4} />
+          </View>
+        </ScrollView>
+      </View>
+      <View style={styles.tabContainer}>
+        <TabView
+          renderTabBar={renderTabBar}
+          navigationState={{ index, routes }}
+          renderScene={renderScene}
+          onIndexChange={setIndex}
+          initialLayout={initialLayout}
+        />
       </View>
     </View>
   )
@@ -20,6 +82,7 @@ const Home = () => {
 export default Home
 
 const styles = StyleSheet.create({
+  page: { flex: 1 },
   profileContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -28,18 +91,9 @@ const styles = StyleSheet.create({
     paddingBottom: 24,
     backgroundColor: 'white'
   },
-  appName: {
-    fontSize: 22,
-    fontFamily: Fonts.POPPINS_MEDIUM,
-    color: '#020202'
-  },
-  desc: {
-    fontFamily: Fonts.POPPINS_LIGHT,
-    color: '#8D92A3'
-  },
-  profile: {
-    width: 50,
-    height: 50,
-    borderRadius: 8
-  }
+  appName: { fontSize: 22, fontFamily: Fonts.POPPINS_MEDIUM, color: '#020202' },
+  desc: { fontFamily: Fonts.POPPINS_LIGHT, color: '#8D92A3' },
+  profile: { width: 50, height: 50, borderRadius: 8 },
+  foodContainer: { flexDirection: 'row', marginVertical: 24, marginLeft: 24 },
+  tabContainer: { flex: 1 }
 })
