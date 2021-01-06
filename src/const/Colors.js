c3c32a29915b78982ff52ecb485a23b557d759b2
Imam Robani <imamrob@gmail.com>
2021-01-07 06:36:49 +0700

    add colors

---
+++ src/const/Colors.js
@@ -0,0 +1,12 @@
+const Colors = {
+  marigold: '#ffc700',
+  black: '#020202',
+  blueyGrey: '#8d92a3',
+  topaz: '#1abc9c',
+  fadedRed: '#d9435e',
+  veryLightPink: '#ececec',
+  brightOrange: '#ff5c00',
+  white: '#ffffff'
+}
+
+export default Colors
