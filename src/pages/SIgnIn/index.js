e54fd56304486eb32ae7f7a824f4272efc9681d7
Imam Robani <imamrob@gmail.com>
2021-01-16 07:15:06 +0700

    create custom hook, useform

--- src/pages/SIgnIn/index.js
+++ src/pages/SIgnIn/index.js
@@ -1,17 +1,38 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import { Header, TextInput, Button, Gap } from '../../components'
+import { useForm } from '../../utils'
 
 const SignIn = ({ navigation }) => {
+  const [form, setForm] = useForm({
+    email: '',
+    password: ''
+  })
+
+  const onSubmit = () => {
+    console.log('form: ', form)
+  }
+
   return (
     <View style={styles.page}>
       <Header title='Sign In' subTitle='Find your best ever meal' />
       <View style={styles.container}>
-        <TextInput label='Email address' placeholder='Type your email address' />
+        <TextInput
+          label='Email address'
+          placeholder='Type your email address'
+          value={form.email}
+          onChangeText={(value) => setForm('email', value)}
+        />
         <Gap height={16} />
-        <TextInput label='Password' placeholder='Type your password' />
+        <TextInput
+          label='Password'
+          placeholder='Type your password'
+          value={form.password}
+          onChangeText={(value) => setForm('password', value)}
+          secureTextEntry
+        />
         <Gap height={24} />
-        <Button text='Sign In' />
+        <Button text='Sign In' onPress={onSubmit} />
         <Gap height={12} />
         <Button
           text='Create New Account'
