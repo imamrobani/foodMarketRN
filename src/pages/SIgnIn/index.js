e6a4d6c2a4f560038d104e7eea28acaee9f5ef22
Imam Robani <imamrob@gmail.com>
2021-01-19 21:29:57 +0700

    login integration

--- src/pages/SIgnIn/index.js
+++ src/pages/SIgnIn/index.js
@@ -1,7 +1,8 @@
-import Axios from 'axios'
 import React from 'react'
 import { StyleSheet, View } from 'react-native'
-import { Header, TextInput, Button, Gap } from '../../components'
+import { useDispatch } from 'react-redux'
+import { Button, Gap, Header, TextInput } from '../../components'
+import { signInAction } from '../../redux/action/auth'
 import { useForm } from '../../utils'
 
 const SignIn = ({ navigation }) => {
@@ -10,14 +11,10 @@ const SignIn = ({ navigation }) => {
     password: ''
   })
 
+  const dispatch = useDispatch()
+
   const onSubmit = () => {
-    Axios.post('http://foodmarket-backend.buildwithangga.id/api/login', form)
-      .then(res => {
-        // console.log('success', res)
-      })
-      .catch(err => {
-        // console.log('error', err)
-      })
+    dispatch(signInAction(form, navigation))
   }
 
   return (
@@ -41,7 +38,7 @@ const SignIn = ({ navigation }) => {
         <Gap height={24} />
         <Button
           text='Sign In'
-          onPress={() => navigation.replace('MainApp')} />
+          onPress={onSubmit} />
         <Gap height={12} />
         <Button
           text='Create New Account'
