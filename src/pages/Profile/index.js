2bd2b9b1a86827cefbae9027fd18535bcd87cabf
Imam Robani <imamrob@gmail.com>
2021-01-31 20:01:55 +0700

    integrate profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,20 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Image, StyleSheet, Text, View } from 'react-native'
 import { ProfileDummy } from '../../assets'
 import { ProfileTabSection } from '../../components/molecules'
 import { Colors, Fonts } from '../../const'
+import { getData } from '../../utils'
 
 const Profile = () => {
+  const [userProfile, setUserProfile] = useState({})
+
+  useEffect(() => {
+    getData('userProfile').then(res => {
+      setUserProfile(res)
+    })
+  }, [])
+
   return (
     <View style={styles.page}>
       <View style={styles.profileDetail}>
         <View style={styles.photo}>
           <View style={styles.borderPhoto}>
-            <Image source={ProfileDummy} style={styles.photoContainer} />
+            <Image source={{ uri: userProfile.profile_photo_url }} style={styles.photoContainer} />
           </View>
         </View>
-        <Text style={styles.name}>Angga Rizky</Text>
-        <Text style={styles.email}>wepanda@gmail.com</Text>
+        <Text style={styles.name}>{userProfile.name}</Text>
+        <Text style={styles.email}>{userProfile.email}</Text>
       </View>
       <View style={styles.content}>
         <ProfileTabSection />
